40b4a51eca2db917d93f1b522d33b0eabfe639b4
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-22 00:31:26 -0500

    Added component for circle bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SciFiComponent } from './components/sci-fi/sci-fi.component';
+import { CircleBarComponent } from './components/circle-bar/circle-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SciFiComponent
+    SciFiComponent,
+    CircleBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/circle-bar/circle-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-bar',
+  templateUrl: './circle-bar.component.html',
+  styleUrls: ['./circle-bar.component.css']
+})
+export class CircleBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
